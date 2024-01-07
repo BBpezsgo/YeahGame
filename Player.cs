8fafca62e9bc0fa82716b6ac3bb476dd31830160
ShixHuntR <destroyer2k5.fb@gmail.com>
2024-01-07 13:04:02 +0100

    update

--- Player.cs
+++ Player.cs
@@ -2,39 +2,43 @@
 {
     public class Player : Entity
     {
-        
-
+        float LastShot = Time.Now;
+        const float Speed = 10;
         public override void Update()
         {
             if (Keyboard.IsKeyPressed('W'))
             {
-                Position.Y -= 10 * Time.Delta;
+                Position.Y -= Speed * 0.5f * Time.Delta;
             }
             if (Keyboard.IsKeyPressed('S'))
             {
-                Position.Y += 10 * Time.Delta;
+                Position.Y += Speed * 0.5f * Time.Delta;
             }
             if (Keyboard.IsKeyPressed('A'))
             {
-                Position.X -= 10 * Time.Delta;
+                Position.X -= Speed * Time.Delta;
             }
             if (Keyboard.IsKeyPressed('D'))
             {
-                Position.X += 10 * Time.Delta;
+                Position.X += Speed * Time.Delta;
             }
             Position.X = Math.Clamp(Position.X, 0, Game.Renderer.Width - 1);
             Position.Y = Math.Clamp(Position.Y, 0, Game.Renderer.Height - 1);
 
-            if (Mouse.IsPressed(MouseButton.Left))
+            if (Mouse.IsPressed(MouseButton.Left)&&
+                Time.Now - LastShot > 0.5f)
             {
                 Vector2 velocity = Mouse.RecordedConsolePosition - Position;
                 velocity = Vector2.Normalize(velocity);
-                velocity *= 15;
+                velocity *= 25;
+                velocity *= new Vector2(1, 0.5f);
                 Projectile newProjectile = new();
                 newProjectile.Position = Position;
                 newProjectile.Velocity = velocity;
                 newProjectile.SpawnedAt = Time.Now;
                 Game.Singleton.projectiles.Add(newProjectile);
+                LastShot = Time.Now;
+
             }
         }
 
