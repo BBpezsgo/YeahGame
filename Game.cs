07b7d5d49d6978931d2c4dd9e6831d6e0fd3edee
BB pezsgő <bazsi5fel2@gmail.com>
2024-01-07 10:28:56 +0100

    time and basic stuff

--- Game.cs
+++ Game.cs
@@ -1,16 +1,46 @@
-﻿using Win32;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Win32;
 
 namespace YeahGame;
 
 public class Game
 {
+    static Game? singleton;
+
+    readonly ConsoleRenderer renderer;
+
+    public static ConsoleRenderer Renderer => singleton!.renderer;
+
+    public Game()
+    {
+        renderer = new ConsoleRenderer();
+        singleton = this;
+    }
+
     public void Start()
     {
-        ConsoleRenderer renderer = new();
+        bool wasResized = false;
+
+        ConsoleListener.KeyEvent += Keyboard.Feed;
+        ConsoleListener.MouseEvent += Mouse.Feed;
+        ConsoleListener.WindowBufferSizeEvent += _ => wasResized = true;
 
         while (true)
         {
-            renderer.ClearBuffer();
+            Time.Tick();
+            Keyboard.Tick();
+            Mouse.Tick();
+
+            if (wasResized)
+            {
+                renderer.RefreshBufferSize();
+                wasResized = false;
+            }
+            else
+            {
+                renderer.ClearBuffer();
+            }
 
             renderer[5, 5] = new ConsoleChar('X');
             renderer.Render();
---
+++ Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YeahGame;
+
+public static class Time
+{
+    static float lastTime;
+    static float now;
+    static float deltaTime;
+
+    public static float Now => now;
+    public static float Delta => deltaTime;
+
+    public static void Tick()
+    {
+        now = (float)DateTime.UtcNow.TimeOfDay.TotalSeconds;
+        deltaTime = now - lastTime;
+        lastTime = now;
+    }
+}
